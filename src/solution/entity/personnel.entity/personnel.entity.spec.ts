bb2580bce2cf571499784546e94200745269a06c
Brahim-Gdoura <142800116+Brahim-Gdoura@users.noreply.github.com>
2025-07-07 10:51:51 +0100

    Delete src directory

--- src/solution/entity/personnel.entity/personnel.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PersonnelEntity } from './personnel.entity';
-
-describe('PersonnelEntity', () => {
-  it('should be defined', () => {
-    expect(new PersonnelEntity()).toBeDefined();
-  });
-});
--- src/solution/entity/solution.entity/solution.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SolutionEntity } from './solution.entity';
-
-describe('SolutionEntity', () => {
-  it('should be defined', () => {
-    expect(new SolutionEntity()).toBeDefined();
-  });
-});
